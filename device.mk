686b871dc2c1cefc6a3936b6044e9de65360b130
ArchieMeng <archiemeng@protonmail.com>
2025-05-17 00:27:06 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -31,6 +31,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/surya/surya-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
